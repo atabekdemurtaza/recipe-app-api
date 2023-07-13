e667cf0039bbe1f52b19b484d1b68c004aedfc65
atabekdemurtaza <mychannel21war@gmail.com>
2023-07-13 17:24:35 +0500

    Linting

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,15 +7,14 @@ from .calc import add, subtract
 
 class CalcTests(TestCase):
     """Test the calc module."""
-
     def test_add_numbers(self):
         """Test adding numbers together."""
-        res = add(5,6)
+        res = add(5, 6)
         self.assertEqual(res, 11)
-    
+        
     def test_subtract_numbers(self):
         """Test subtracting numbers."""
-        res = subtract(10,15)
+        res = subtract(10, 15)
         self.assertEqual(res, 5)
 
-        
+
